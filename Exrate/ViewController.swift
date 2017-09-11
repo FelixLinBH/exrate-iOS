3d37c4ff67c88aec2c351dc651eb94c5c7c8ad42
Felix.lin <fly_81211@hotmail.com>
2017-09-11 23:39:35 +0800

    init

--- Exrate/ViewController.swift
+++ Exrate/ViewController.swift
@@ -7,12 +7,49 @@
 //
 
 import UIKit
+import SocketIO
+let socket = SocketIOClient(socketURL: URL(string: "http://18.221.63.20:80")!, config: [.log(true), .compress])
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        socket.on(clientEvent: .connect) {data, ack in
+            print("socket connected")
+        }
+        
+        socket.on("connect") {data, ack in
+            print("socket connected")
+        }
+        socket.on(clientEvent: .error) { (data, error) in
+            print(data)
+            print(error)
+        }
+        socket.on("message") {data, ack in
+            print(data[0])
+//            if let cur = data[0] as? Double {
+//                socket.emitWithAck("canUpdate", cur).timingOut(after: 0) {data in
+//                    socket.emit("update", ["amount": cur + 2.50])
+//                }
+//                
+//                ack.with("Got your currentAmount", "dude")
+//            }
+        }
+        
+        socket.on("event") {data, ack in
+            print(data[0])
+            //            if let cur = data[0] as? Double {
+            //                socket.emitWithAck("canUpdate", cur).timingOut(after: 0) {data in
+            //                    socket.emit("update", ["amount": cur + 2.50])
+            //                }
+            //
+            //                ack.with("Got your currentAmount", "dude")
+            //            }
+        }
+        
+        socket.connect()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Exrate' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Exrate
+  pod 'Socket.IO-Client-Swift', '~> 11.1.2'
+  pod 'LZCompression'
+  target 'ExrateTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ExrateUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
